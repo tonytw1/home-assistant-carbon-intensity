7a03bf5fc6f7312ded4cc730110da46ab5893584
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-10 09:01:25 +0100

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Home Assistant Great Britain carbon insentisty componment
+
+Adds support for the [National Grid's Carbon Intensity API](http://carbonintensity.org.uk).
+The carbon intensity forecast gives an indication of the CO2 emissions related to electricity generation in Great Britain.
+This component potentially allows Home Assistant to schedule jobs to minimise emissions.
+
+![Example](example.png)
+
+
+
+
---
+++ great_britain_carbon_intensity.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+import requests
+import json
+
+from homeassistant.const import CONF_ELEVATION
+from homeassistant.core import callback
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import (async_track_point_in_utc_time, async_track_utc_time_change)
+
+_LOGGER = logging.getLogger(__name__)
+
+REQUIREMENTS = []
+
+DOMAIN = 'great_britain_carbon_intensity'
+
+FORECAST = "forecast"
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    carbon = GreatBritainCarbonIntensity(hass)
+    return True
+
+class GreatBritainCarbonIntensity(Entity):
+
+    entity_id = DOMAIN + ".instance"
+
+    def __init__(self, hass):
+        self.hass = hass
+        self._state = None
+        async_track_utc_time_change(hass, self.timer_update, second=30)
+
+    @property
+    def name(self):
+        return 'Carbon intensity'
+
+    @property
+    def state(self):
+        return str(self._state)
+
+    @property
+    def state_attributes(self):
+        return {
+            FORECAST: self._state
+        }
+
+    @property
+    def unit_of_measurement(self):
+        return "gCO2/kWh"
+
+    def update(self):
+        _LOGGER.warning("Updating")
+        resp = requests.get("https://api.carbonintensity.org.uk/intensity")
+        response_json = resp.json()
+        forecast = int(response_json['data'][0]['intensity']['forecast'])
+        self._state = forecast
+
+    @callback
+    def timer_update(self, time):
+        _LOGGER.warning("timer_update")
+        self.update()
+        self.async_schedule_update_ha_state()
+
